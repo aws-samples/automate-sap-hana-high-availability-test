765f416b58ef001a19a2787e22791b32275b885b
Guilherme <sesterhg@amazon.com>
2023-01-27 12:26:52 -0500

    updating report generation to store files with date and time of run

--- jenkins/stages/run_ha_test/generate_report.sh
+++ jenkins/stages/run_ha_test/generate_report.sh
@@ -3,7 +3,29 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+location=$(PWD)
+osFamily=`uname`
+processorArchitecture=`uname -p`
+
 cd reports
 
-rm -f report.html
-./report_generator_linux_amd64 -log-file=../log.json
+if [[ "$osFamily" == "Darwin" ]]; then
+    if [[ "$processorArchitecture" == "arm" ]]; then
+        ./bin/report_generator_darwin_arm64 -log-file=$location/log.json
+    else
+        ./bin/report_generator_darwin_amd64 -log-file=$location/log.json
+    fi
+
+else
+    if [[ "$processorArchitecture" == "arm" ]]; then
+        ./bin/report_generator_linux_arm64 -log-file=$location/log.json
+    else
+        ./bin/report_generator_linux_amd64 -log-file=$location/log.json
+    fi
+fi
+
+fileName=$(ls -t | awk 'NR==1{print $1}')
+fileFullPath="$location/reports/$fileName"
+echo "Test finished! Your report is stored in $fileFullPath"
+
+cd ..
